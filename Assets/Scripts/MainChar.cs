0693d72e9d88528f27fd8cba4609a7ea19b86a73
shvabrey <egr.bar152@gmail.com>
2020-04-23 20:53:42 +0700

    стрельба пустыми кубиками + перемещение. прыжки работают странно

--- Assets/Scripts/MainChar.cs
+++ Assets/Scripts/MainChar.cs
@@ -7,9 +7,9 @@ public class MainChar : MonoBehaviour
     Rigidbody2D obj;
     public Vector2 speed = new Vector2(10, 0);
     private Vector2 movement;
-    public bool isgrounded;
+    public bool isgrounded = false;
     public Vector2 jump = new Vector2(0, 10);
-    public Vector2 powerOfShot = new Vector2(10, 10);
+    public Vector2 powerOfShot = new Vector2(10, 5);
     public GameObject shot;
     void Start()
     {
@@ -20,23 +20,24 @@ public class MainChar : MonoBehaviour
     {
         float inputX = Input.GetAxis("Horizontal");
         movement = new Vector2(speed.x * inputX, obj.velocity.y);
-        //ниже жалкая попытка рэйкаста для определения земли
-        isgrounded = (Physics2D.Raycast(new Vector2(obj.transform.position.x - 0.5f, obj.transform.position.y - 0.5f), Vector2.down, 0.1f)) || (Physics2D.Raycast(new Vector2(obj.transform.position.x + 0.5f, obj.transform.position.y - 0.5f), Vector2.down, 0.1f));
     }
 
     void FixedUpdate()
     {
-        if (isgrounded == true)
+        GetComponent<Rigidbody2D>().velocity = movement;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            GetComponent<Rigidbody2D>().velocity = movement;
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            //ниже жалкая попытка рэйкаста для определения земли
+            isgrounded = (Physics2D.Raycast(new Vector2(obj.transform.position.x - 0.5f, obj.transform.position.y - 0.5f), Vector2.down, 0.1f)) || (Physics2D.Raycast(new Vector2(obj.transform.position.x + 0.5f, obj.transform.position.y - 0.5f), Vector2.down, 0.1f));
+            if (isgrounded == true)
             {
                 obj.AddForce(jump, ForceMode2D.Impulse);
             }
         }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject bullet = Instantiate(shot);
+            GameObject bullet = Instantiate(shot, new Vector3(transform.position.x, transform.position.y), Quaternion.identity);
             bullet.GetComponent<Rigidbody2D>().AddForce(powerOfShot, ForceMode2D.Impulse);
         }
     }
