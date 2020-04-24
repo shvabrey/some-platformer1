a45eafe9dde422d34167ad7280e0e62eee793933
assasin2000100 <fmail2000100@gmail.com>
2020-04-24 17:11:10 +0700

    no message

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float damage = 10f;
+    void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            GameObject obj = other.gameObject;
+            obj.GetComponent<Enemy>().GetHit(damage);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/MainChar.cs
+++ Assets/Scripts/MainChar.cs
@@ -45,7 +45,7 @@ public class MainChar : MonoBehaviour
         if ((Input.GetKeyDown(KeyCode.UpArrow)) || (Input.GetKeyDown(KeyCode.W)))
         {
             //ниже жалкая попытка рэйкаста для определения земли
-            isgrounded = (Physics2D.Raycast(new Vector2(obj.transform.position.x - 0.15f, obj.transform.position.y - 0.5f), Vector2.down, 0.1f)) || (Physics2D.Raycast(new Vector2(obj.transform.position.x + 0.15f, obj.transform.position.y - 0.5f), Vector2.down, 0.1f));
+            isgrounded = (Physics2D.Raycast(new Vector2(obj.transform.position.x - 0.15f, obj.transform.position.y - 0.5f), Vector2.down, 0.2f)) || (Physics2D.Raycast(new Vector2(obj.transform.position.x + 0.15f, obj.transform.position.y - 0.5f), Vector2.down, 0.2f));
             if (isgrounded == true)
             {
                 obj.AddForce(jump, ForceMode2D.Impulse);
@@ -67,13 +67,20 @@ public class MainChar : MonoBehaviour
             }
             
         }
-
-    }
-
-    void FixedUpdate()
-    {
-        
-
-        
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            for (int i = 7; i >= 0; i--)
+            {
+                GameObject bullet = Instantiate(shot, new Vector3(transform.position.x, transform.position.y - 0.3f, -1f), Quaternion.identity);
+                if (faceRight == true)
+                {
+                    bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(8f, 10f), Random.Range(-1f, 1f)), ForceMode2D.Impulse);
+                }
+                else
+                {
+                    bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-8f, -10f), Random.Range(-1f, 1f)), ForceMode2D.Impulse);
+                }
+            }
+        }
     }
 }
